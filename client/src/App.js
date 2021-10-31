bd35ea6141299e0c90254297b82a6f38abcac67b
Jesse Lee <jesselee@Jesses-Air.hsd1.ca.comcast.net>
2021-10-30 20:01:51 -0700

    fixed client dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import { Wrapper } from "./components/Wrapper";
-import Routes from "./Routes";
-// import {GunProvider} from "use-gun"
+import logo from './logo.svg';
+import './App.css';
 
-const App = ({ children }) => {
-  // const peers = ["http://localhost:8080/gun"];
+function App() {
   return (
-    // <GunProvider peerUrls={peers}>
-      <Wrapper>
-        <Routes />
-      </Wrapper>
-    // {/* </GunProvider> */}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- client/src/utils/truncate.js
+++
@@ -1,1 +0,0 @@
-export const truncate = (input) => input.length > 45 ? `${input.substring(0, 45)}...` : input;
